61c86e2061fcba734e237f518fe341183162b5b9
Aaron Hammond <firefox256@gmail.com>
2025-05-30 19:50:28 -0600

    updates

--- tools/try1.js
+++ tools/try1.js
@@ -1,5 +1,5 @@
-globalThis.deepclone = 
-function (obj, hash = new WeakMap()) {
+
+globalThis.deepclone = function (obj, hash = new WeakMap()) {
     if (obj === null || typeof obj !== 'object') return obj;
     if (hash.has(obj)) return hash.get(obj); // Cyclic reference
     const clone = Array.isArray(obj) ? [] : {};
@@ -12,9 +12,31 @@ function (obj, hash = new WeakMap()) {
     return clone;
 };
 
-var Try1Properties = {
-    x: 123,
-    y: 124
+
+        class c1
+        {
+            constructor()
+            {
+                console.log("here at c1")
+            }
+        }
+        var v1 = new c1();
+        var printthis =() =>
+        {
+            console.log("print this")
+        }
+        
+       
+       
+        
+        
+
+        
+
+        var Try1Properties = {
+            x : 123,
+            y : 124
+
 };
 
 function Try1printout(self) {
@@ -39,32 +61,54 @@ function Try1Constructor(self) {
     self.y = 3333
 }
 
+
+var t1 = Try1New(); Try1Constructor(t1);; // Removed to match the target output
+Try1printout(t1); // Removed to match the target output
+
+
 var out1Properties = {
 
 };
 
-function out1func(self) {
-    console.log("out1 log")
+function out1func(self, arg1) {
+    console.log("out1 log " + arg1)
 }
 
 function out1New() {
     return deepclone(out1Properties);
 }
 
-function out1Constructor(self) {
+function out1Constructor(self, a, b) {
+    self.a = a;
+    self.b = b;
+}
+
+var out2Properties = {
+
+};
+
+function out2func1(self) {
+    console.log("out2 log ")
+}
+
+function out2New() {
+    return deepclone(out2Properties);
+}
+
+function out2Constructor(self) {
 }
 
 var Try2Properties = {
-    x: 123,
-    y: 124,
-    title: "hi there"
+
 };
 
 function Try2printout(self) {
     Try1printx(self);
     Try1printy(self);
     Try2printtitle(self);
-    out1func(self);
+    out1func(self, "an argument");
+    out2func1(self);
+    printthis();
 }
 
 function Try2printtitle(self) {
@@ -75,11 +119,13 @@ function Try2New() {
     return deepclone(Try2Properties);
 }
 
-function Try2Constructor(self) {
+function Try2Constructor(self, maybe) {
+    console.log("maybe here 1")
     Try1Constructor(self);
-    out1Constructor(self);
+    out1Constructor(self, 222, 333);
+    out2Constructor(self);
     self.title += " and more";
 }
 
 var t2 = Try2New(); Try2Constructor(t2);
-        Try2printout(t2);
+        Try2printout(t2)
